3fa712b346a2d252b3c4c088a3defecfcde2111a
Quinn Roberts <quinn@jelly-snakes.com>
2018-12-16 12:34:32 +1100

    Switch over miners to use entities rather than seperate classes

--- src/main/java/org/terasology/depths/caveGen/miner/MinerBuildSystem.java
+++ src/main/java/org/terasology/depths/caveGen/miner/MinerBuildSystem.java
@@ -25,8 +25,11 @@ import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.logic.delay.DelayManager;
 import org.terasology.logic.delay.PeriodicActionTriggeredEvent;
+import org.terasology.logic.location.Location;
+import org.terasology.logic.location.LocationComponent;
 import org.terasology.logic.players.LocalPlayer;
 import org.terasology.logic.players.event.OnPlayerSpawnedEvent;
+import org.terasology.math.geom.Vector3f;
 import org.terasology.math.geom.Vector3i;
 import org.terasology.registry.In;
 import org.terasology.utilities.random.FastRandom;
@@ -45,7 +48,7 @@ public class MinerBuildSystem extends BaseComponentSystem {
     /**
      * Number of empty cells around the miner to kill it
      */
-    private static final int KILL_NUM = 23;
+    private static final int KILL_NUM = 20;
     /**
      * Chance to spawn in a new miner each frame
      */
@@ -60,13 +63,15 @@ public class MinerBuildSystem extends BaseComponentSystem {
      */
     private static final int UPDATE_RATE = 2000;
     private static final String UPDATE_ID = "depthsMinerUpdate";
+    private static final String MINER_PREFAB = "GooeysDepths:Miner";
 
     private final FastRandom random = new FastRandom();
     private Block airBlock;
 
-    private List<Miner> miners = new ArrayList<>();
-    private List<Miner> toRemove = new ArrayList<>();
-    private List<Miner> toAdd = new ArrayList<>();
+    private List<EntityRef> miners = new ArrayList<>();
+    private List<EntityRef> toRemove = new ArrayList<>();
+    private List<EntityRef> toAdd = new ArrayList<>();
+
 
     @In
     private DelayManager delayManager;
@@ -82,7 +87,10 @@ public class MinerBuildSystem extends BaseComponentSystem {
     @Override
     public void postBegin() {
         airBlock = blockManager.getBlock(BlockManager.AIR_ID);
-        miners.add(new Miner(1, -1, 1));
+        EntityRef miner = entityManager.create(MINER_PREFAB);
+        LocationComponent locationComponent = miner.getComponent(LocationComponent.class);
+        locationComponent.setWorldPosition(new Vector3f(1, -1, 1));
+        miners.add(miner);
 
         EntityBuilder entityBuilder = entityManager.newBuilder();
         entityBuilder.setPersistent(true);
@@ -106,10 +114,11 @@ public class MinerBuildSystem extends BaseComponentSystem {
     private void processMiners() {
         logger.info("Processing " + miners.size() + " miners.");
         /* Process each miner */
-        for (Miner miner : miners) {
+        for (EntityRef miner : miners) {
             /* If the miner cannot survive */
             if (!isMinerSafe(miner)) {
                 toRemove.add(miner);
+                miner.destroy();
                 continue;
             }
             trySpawnClone(miner);
@@ -125,12 +134,14 @@ public class MinerBuildSystem extends BaseComponentSystem {
         toAdd.clear();
     }
 
-    private boolean isMinerSafe(Miner miner) {
+    private boolean isMinerSafe(EntityRef miner) {
+        Vector3f location = miner.getComponent(LocationComponent.class).getWorldPosition();
+        Vector3i minerLocation = new Vector3i(location);
         int empty = 0;
         for (int x = -1; x <= 1; x++) {
             for (int y = -1; y <= 1; y++) {
                 for (int z = -1; z <= 1; z++) {
-                    if (worldProvider.getBlock(x + miner.pos.x, y + miner.pos.y, z + miner.pos.z) == airBlock) {
+                    if (worldProvider.getBlock(x + minerLocation.x, y + minerLocation.y, z + minerLocation.z) == airBlock) {
                         empty++;
                     }
                 }
@@ -139,37 +150,46 @@ public class MinerBuildSystem extends BaseComponentSystem {
         return empty < KILL_NUM;
     }
 
-    private void trySpawnClone(Miner miner) {
+    private void trySpawnClone(EntityRef miner) {
         if (random.nextFloat() < CLONE_CHANCE) {
-            toAdd.add(new Miner(miner));
+            EntityRef newMiner = entityManager.create(MINER_PREFAB);
+            LocationComponent newMinerComponent = newMiner.getComponent(LocationComponent.class);
+            LocationComponent minerComponent = miner.getComponent(LocationComponent.class);
+
+            newMinerComponent.setWorldPosition(minerComponent.getWorldPosition());
+            toAdd.add(newMiner);
         }
     }
 
-    private Vector3i getRandomDirection() {
+    private Vector3f getRandomDirection() {
         switch (random.nextInt(6)) {
             case 0:
-                return Vector3i.up();
+                return Vector3f.up();
             case 1:
-                return Vector3i.down();
+                return Vector3f.down();
             case 2:
-                return Vector3i.north();
+                return Vector3f.north();
             case 3:
-                return Vector3i.west();
+                return Vector3f.west();
             case 4:
-                return Vector3i.east();
+                return Vector3f.east();
             case 5:
-                return Vector3i.south();
+                return Vector3f.south();
             default:
-                return Vector3i.zero();
+                return Vector3f.zero();
         }
     }
 
-    private void moveMiner(Miner miner) {
-        miner.pos.add(getRandomDirection());
+    private void moveMiner(EntityRef miner) {
+        LocationComponent component = miner.getComponent(LocationComponent.class);
+        Vector3f pos = component.getWorldPosition();
+        pos.add(getRandomDirection());
+        component.setWorldPosition(pos);
     }
 
-    private void mineBlock(Miner miner) {
-        worldProvider.setBlock(miner.pos, airBlock);
+    private void mineBlock(EntityRef miner) {
+        LocationComponent component = miner.getComponent(LocationComponent.class);
+        worldProvider.setBlock(new Vector3i(component.getWorldPosition()), airBlock);
     }
 
     /**
